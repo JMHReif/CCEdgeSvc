b4aa42f4fd85176989301b200d2c8642ccf24805
Jennifer Reif <jennifer@thehecklers.org>
2017-09-17 15:46:40 -0500

    Cleaned up code. Added lombok.

--- src/main/java/com/example/Quote.java
+++ src/main/java/com/example/Quote.java
@@ -1,40 +1,19 @@
 package com.example;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 /**
  * Created by markheckler on 12/17/16.
  * Updated by jmhreif on 09/14/17.
  */
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Quote {
     private Long id;
-    private String text;
-    private String source;
-
-    public Quote() {
-    }
-
-    public Quote(String text, String source) {
-        this.text = text;
-        this.source = source;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    @Override
-    public String toString() {
-        return "Quote{" +
-                "id=" + id +
-                ", text='" + text + '\'' +
-                ", source='" + source + '\'' +
-                '}';
-    }
+    @NonNull
+    private String text, source;
 }
