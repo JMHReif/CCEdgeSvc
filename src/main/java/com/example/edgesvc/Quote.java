c2f630623564bc1e60dc59fd0492d8ab0c1e1725
Jennifer Reif <jennifer@thehecklers.org>
2017-09-19 20:37:42 -0500

    Added missing constructor

--- src/main/java/com/example/edgesvc/Quote.java
+++ src/main/java/com/example/edgesvc/Quote.java
@@ -8,6 +8,9 @@ public class Quote {
     private Long id;
     private String text, source;
 
+    public Quote() {
+    }
+
     public Quote(Long id, String text, String source) {
         this.id = id;
         this.text = text;
